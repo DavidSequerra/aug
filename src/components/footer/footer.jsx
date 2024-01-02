f96d648f1ebde2292723d7b4d5b91a48b528fc71
limor kabrity <limorkab@gmail.com>
2024-01-02 19:25:33 +0200

    footer css

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 function Footer() {
     return (
-        <p>This amazing website was created by Aug 23 group!</p>
+        <>
+            <p className="footer">This amazing website was created by Aug 23 group!</p>
+        </>
     )
 }
 export default Footer
