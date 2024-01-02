00f4365957a6001c49a881037c41d8fde36eb980
devorahgertler <118658262+devorahgertler@users.noreply.github.com>
2024-01-02 19:27:12 +0200

    Updated with react bootstrap

--- src/components/Devorah.jsx
+++ src/components/Devorah.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Devorah = () => {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '30rem' }}>
             <Card.Body>
                 <Card.Title>About Devorah</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">ITC Student, Full-Stack Development</Card.Subtitle>
                 <Card.Text>
                     Fast Facts:
-                    I live in Beit Shemesh.
-                    I work for an EdTech company.
-                    I am a mother of a 4 and 1.5 year old.
-                    I love to bake!
+                    <ul>
+                        <li>I live in Beit Shemesh.</li>
+                        <li>I work for an EdTech company.</li>
+                        <li>I am a mother of a 4 and 1.5 year old.</li>
+                        <li> I love to bake!</li>
+                    </ul>
                 </Card.Text>
             </Card.Body>
         </Card>
