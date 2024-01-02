1ef651623d5d35fa9426e01e1a5f809d7ce14b8c
devorahgertler <118658262+devorahgertler@users.noreply.github.com>
2024-01-02 19:13:45 +0200

    Fast facts

--- src/components/Devorah.jsx
+++ src/components/Devorah.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
 
 const Devorah = () => {
-
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>About Devorah</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">ITC Student, Full-Stack Development</Card.Subtitle>
+                <Card.Text>
+                    Fast Facts:
+                    I live in Beit Shemesh.
+                    I work for an EdTech company.
+                    I am a mother of a 4 and 1.5 year old.
+                    I love to bake!
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Devorah
